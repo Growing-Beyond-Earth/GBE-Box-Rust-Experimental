317f059fa103d9c353907be948bf80cf1bfe3212
Jordan Cisneros <hotdog9955@gmail.com>
2023-06-30 15:48:33 -0400

    added pwm test

--- src/main.rs
+++ src/main.rs
@@ -2,14 +2,14 @@
 #![no_main]
 #![feature(type_alias_impl_trait)]
 
-
 use embassy_executor::Spawner;
-use embassy_rp::{gpio,Peripherals};
+use embassy_rp::{gpio,Peripherals,pwm::{Pwm,Config}};
 use embassy_time::{Duration, Timer};
 use gpio::{Level, Output};
 use {defmt_rtt as _, panic_probe as _};
 use once_cell::sync::Lazy;
 
+// this variable holds the peripheral access with lazy initialization. Memory is allocated at compile time but the value is set at runtime
 static P:Lazy<Peripherals> = Lazy::new(|| {
     embassy_rp::init(Default::default())
 });
@@ -28,18 +28,22 @@ async fn blink1() -> !{
 
 
 #[embassy_executor::task]
-async fn blink2() -> !{
-    let mut led = Output::new(&P.PIN_1, Level::Low);
+async fn startup_led() -> (){
+    let mut pwm_config: Config = Default::default();
+    pwm_config.top = 0x8000;
+    pwm_config.compare_a = 8;
+    
+    let mut neopixel = Pwm::new_output_a(&P.PWM_CH0, &P.PIN_0, pwm_config.clone());
     loop{
-        led.set_high();
-        Timer::after(Duration::from_millis(500)).await;
-        led.set_low();
-        Timer::after(Duration::from_millis(500)).await;
+        pwm_config.compare_a = pwm_config.compare_a.rotate_left(1);
+        neopixel.set_config(&pwm_config);
+        Timer::after(Duration::from_millis(1000)).await;
     }
+
 }
 
 #[embassy_executor::main]
 async fn main(spawner: Spawner) {
-    spawner.spawn(blink1()).unwrap();
-    spawner.spawn(blink2()).unwrap();
+    // spawner.spawn(blink1()).unwrap();
+    spawner.spawn(startup_led()).unwrap();
 }
