1f4a31f2bbc5613ef0a1b21c5d42f3c93baa5a7a
Jordan Cisneros <hotdog9955@gmail.com>
2023-06-30 11:35:40 -0400

    first function async

--- src/main.rs
+++ src/main.rs
@@ -4,23 +4,50 @@
 
 use defmt::*;
 use embassy_executor::Spawner;
-use embassy_rp::gpio;
+use embassy_rp::{gpio,peripherals::{PIN_0,PIN_1}};
 use embassy_time::{Duration, Timer};
 use gpio::{Level, Output};
 use {defmt_rtt as _, panic_probe as _};
 
-#[embassy_executor::main]
-async fn main(_spawner: Spawner) {
-    let p = embassy_rp::init(Default::default());
-    let mut led = Output::new(p.PIN_25, Level::Low);
-
-    loop {
-        info!("led on!");
+#[embassy_executor::task]
+async fn blink1(mut led:Output<'static,PIN_0>) -> !{
+    loop{
+        led.set_low();
+        Timer::after(Duration::from_secs(1)).await;
         led.set_high();
         Timer::after(Duration::from_secs(1)).await;
 
-        info!("led off!");
+    }
+}
+
+#[embassy_executor::task]
+async fn blink2(mut led:Output<'static,PIN_1>) -> !{
+    loop{
         led.set_low();
+        Timer::after(Duration::from_millis(500)).await;
+        led.set_high();
+        Timer::after(Duration::from_millis(500)).await;
+
+    }
+}
+
+
+#[embassy_executor::main]
+async fn main(spawner: Spawner) {
+    let p = embassy_rp::init(Default::default());
+    let led = Output::new(p.PIN_0, Level::Low);
+
+    let led2 = Output::new(p.PIN_1, Level::Low);
+
+    let thing = blink1(led);
+    let thing2 = blink2(led2);
+
+
+    spawner.spawn(thing).unwrap();
+    spawner.spawn(thing2).unwrap();
+
+    loop {
         Timer::after(Duration::from_secs(1)).await;
     }
+
 }
